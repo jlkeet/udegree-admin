60ca0e285cd70e1495e7a7c1e76528cbc5c72ae3
Jackson <jackson.keet@mac.com>
2022-06-05 16:32:41 +1200

    First commit of Admin

---
+++ src/app/@core/data/courses.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  
+    messages: AngularFireList<any>;
+   public courses;
+  
+   constructor(private http: HttpClient, private db: AngularFireDatabase) { }
+
+   /**
+     * Get All tickers from firebase
+     */
+    getMessages() {
+    return new Promise(resolve => {
+      this.db.list('/').valueChanges().subscribe(result => { resolve(result) });
+         })
+    }
+
+//     searchFirebase() {
+//     this.db.list('/').query
+//     .orderByChild("serie").equalTo("0")
+//     .on('value',(data) => {
+//     data.forEach((child) => {
+//     console.log(child.key, child.val());
+//   });
+//   })
+// }
+}
+
